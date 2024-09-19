1a9312faaa08520b841cdfe2f251c95485d2727f
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-09-19 21:38:40 +0530

    POTD

---
+++ September-2024/19_different_ways_to_add_parenthesis.cpp
@@ -0,0 +1,35 @@
+// 241. Different Ways to Add Parentheses
+
+class Solution {
+public:
+    vector<int> solve(string s){
+        vector<int> result;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='+' || s[i]=='-' || s[i]=='*'){
+                vector<int> left = solve(s.substr(0, i));
+                vector<int> right = solve(s.substr(i+1));
+
+                // catch
+                for(auto &x: left){
+                    for(auto &y: right){
+                        if(s[i]=='+')
+                            result.push_back(x+y);
+                        else if(s[i]=='-')
+                            result.push_back(x-y);
+                        else
+                            result.push_back(x*y);
+                    }
+                }
+            }
+        }
+
+        if(result.empty()) result.push_back(stoi(s));
+
+        return result;
+    }
+
+    vector<int> diffWaysToCompute(string expression) {
+        return solve(expression);
+    }
+};
